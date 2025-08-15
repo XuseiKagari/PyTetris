05e599968897ebacda44f65fc09f58880d046bd0
XuseiKagari <96327686+XuseiKagari@users.noreply.github.com>
2025-08-15 23:56:12 +0400

    Add files via upload

--- Tetris.py
+++ Tetris.py
@@ -1,185 +1,202 @@
 import pygame
-import random, time, sys
-from pygame.locals import *
+import random, time
+import numpy as np
 
-screen_x, screen_y = 600, 500
-cell, field_h, field_w = 20, 20, 10
-initialCord_x = (screen_x - cell * field_w) / 2
-initialCord_y = (screen_y - cell * field_h) / 2
-empty = "x"
-pygame.init()
-screen = pygame.display.set_mode((screen_x, screen_y))
-fps_clock = pygame.time.Clock()
-fps = 25
-fall_speed = 0.5
-colors = ((0, 0, 225), (0, 225, 0), (225, 0, 0), (225, 225, 0)) # синий, зеленый, красный, желтый
-lightcolors = ((30, 30, 255), (50, 255, 50), (255, 30, 30), (255, 255, 30)) # светло-синий, светло-зеленый, светло-красный, светло-желтый
+class FigureStorage:
+    def __init__(self):
+        self.__figure = []
 
-def main():
-    running = True
-    fps_clock.tick(fps)
-    playingField = creatPF()
-    nextFigure = {"x": initialCord_x + (cell * field_w) / 2, "y": initialCord_y + 20, "Figure": NextFigure()}
-    fallingFig = {"x": initialCord_x + (cell * field_w) / 2, "y": initialCord_y + 20, "Figure": RandomFigure()}
-    going_down = False
-    going_left = False
-    going_right = False
+    def set_figure(self, idx, state):
+        self.__figure[idx] = state
 
-    last_move_down = time.time()
-    last_side_move = time.time()
-    last_fall = time.time()
-    points = 0
+class Figure:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.color = random.randint(0, 3)
+        self.figure_type = self.random_figure()
 
-    while running:
+    @staticmethod
+    def random_figure():
+        figures_cord = [
+            # линия
+            [["oooo"],
+             ["xxxx"],
+             ["oooo"],
+             ["oooo"]],
+            # L-образная
+            [["xoo"],
+             ["xoo"],
+             ["xxo"]],
+            # обратная L-образная
+            [["oox"],
+             ["oox"],
+             ["oxx"]],
+            # квадрат
+            [["xx"],
+             ["xx"]],
+            # Z-образная
+            [["xxo"],
+             ["oxx"],
+             ["ooo"]],
+            # обратная Z-образная
+            [["oxx"],
+             ["xxo"],
+             ["ooo"]],
+            # T-образная
+            [["oxo"],
+             ["xxx"],
+             ["ooo"]]
+        ]
+        figure = random.choice(figures_cord)
+        return figure
 
-        if fallingFig == None:
-            fallingFig = nextFigure
-            nextFigure = {"x": initialCord_x + (cell * field_w) / 2, "y": initialCord_y + 20, "Figure": NextFigure()}
-            last_fall = time.time()
+
+    def left_move(self):
+        if self.check_pos(playingField, next_x=-1):
+            self.x -= 1
+            going_left = True
+            going_right = False
+            last_side_move = time.time()
+
+    def right_move(self):
+        if self.check_pos(playingField, next_x=1):
+            self.x += 1
+            going_right = True
+            going_left = False
+            last_side_move = time.time()
+
+    def rotate(self, k=3):
+        a = np.array(self.figure_type)
+        figure_type = np.rot90(a, k).tolist()
 
 
-        DrawFallingFigure(fallingFig)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            elif event.type == KEYDOWN:
+    def fast_falling(self):
+        going_down = True
+        if self.check_pos(playingField, next_y=1):
+            self.y += 1
+            last_move_down = time.time()
 
-                # перемещение фигуры вправо и влево
-                if event.key == K_LEFT and checkPos(playingField, fallingFig, next_x=-1):
-                    fallingFig['x'] -= cell
-                    going_left = True
-                    going_right = False
-                    last_side_move = time.time()
+    # мгновенный сброс вниз
+    def instant_falling(self):
+        going_down = False
+        going_left = False
+        going_right = False
+        for i in range(cell, field_h * cell):
+            if not self.check_pos(playingField, next_y=i):
+                break
+            self.y += i - 1
 
-                elif event.key == K_RIGHT and checkPos(playingField, fallingFig, next_x=1):
-                    fallingFig['x'] += cell
-                    going_right = True
-                    going_left = False
-                    last_side_move = time.time()
-
-                # поворачиваем фигуру, если есть место
-                elif event.key == K_UP:
-                    fallingFig['rotation'] = (fallingFig['rotation'] + 1) % len(figures[fallingFig['shape']])
-                    if not checkPos(playingField, fallingFig):
-                        fallingFig['rotation'] = (fallingFig['rotation'] - 1) % len(figures[fallingFig['shape']])
-
-                # ускоряем падение фигуры
-                elif event.key == K_DOWN:
-                    going_down = True
-                    if checkPos(playingField, fallingFig, next_y=1):
-                        fallingFig['y'] += cell
-                    last_move_down = time.time()
-
-                # мгновенный сброс вниз
-                elif event.key == K_RETURN:
-                    going_down = False
-                    going_left = False
-                    going_right = False
-                    for i in range(cell, field_h * cell):
-                        if not checkPos(playingField, fallingFig, next_y=i):
-                            break
-                        fallingFig['y'] += i - cell
-
+    def free_fall(self):
         if time.time() - last_fall > fall_speed:  # свободное падение фигуры
-            if not checkPos(playingField, fallingFig, next_y=1):  # проверка "приземления" фигуры
-                addToCup(playingField, fallingFig)  # фигура приземлилась, добавляем ее в содержимое стакана
-
+            if not self.check_pos(playingField, next_y=1):  # проверка "приземления" фигуры
                 fallingFig = None
-            else: # фигура пока не приземлилась, продолжаем движение вниз
-                fallingFig['y'] += cell
+            else:  # фигура пока не приземлилась, продолжаем движение вниз
+                self.y += 1
                 last_fall = time.time()
 
-        pygame.display.flip()
-        drawingPF(playingField)
+    def check_pos(self, playingField, next_x=0, next_y=0):
 
-    pygame.quit()
-
-
-def creatPF():
-    PF = []
-    for x in range(field_w):
-        PF.append([])
-        for y in range(field_h):
-            PF[x].append(empty)
-    return PF
+        for block_x, block_y in self.figure_type:
+            if self.y + block_y >= 19 or playingField[self.x + block_x + next_x][self.y + block_y + next_y] != "X":
+                return False
+        return True
 
 
+class PlayingField:
+    __playing_field = []
+    __field_h = 20
+    __field_w = 10
+    __cell = 20
+    colors = ((0, 0, 225),      # синий
+              (0, 225, 0),      # зеленый
+              (225, 0, 0),      # красный
+              (225, 225, 0))    # желтый
 
-def drawingPF(PF):
-    pygame.draw.rect(screen, "grey",
-                     (initialCord_x, initialCord_y, cell * field_w, cell * field_h))
+    light_colors = ((30, 30, 255),   # светло-синий
+                    (50, 255, 50),   # светло-зеленый
+                    (255, 30, 30),   # светло-красный
+                    (255, 255, 30))  # светло-желтый
 
-    for x in range(len(PF)):
-        for y in range(len(PF[x])):
-            pygame.draw.rect(screen, "black",
-                     (initialCord_x + cell * x, initialCord_y + cell * y, cell, cell), 1)
-            drawBlock(initialCord_x + cell * x, initialCord_y + cell * y, PF[x][y])
+    def __init__(self, screen_x, screen_y):
+        self.initial_cord_x = (screen_x - self.__cell * self.__field_w) / 2
+        self.initial_cord_y = (screen_y - self.__cell * self.__field_h) / 2
+        for x in range(self.__field_w):
+            self.__playing_field.append([])
+            for y in range(self.__field_h):
+                self.__playing_field[x].append("X")
+
+    def drawing_playing_field(self, screen):
+        pygame.draw.rect(screen, "grey",
+                         ( self.initial_cord_x,  self.initial_cord_y,  self.__cell * self.__field_w, self.__cell * self.__field_h))
+
+        for x in range(len(self.__playing_field)):
+            for y in range(len(self.__playing_field[x])):
+                pygame.draw.rect(screen, "black",
+                                 ( self.initial_cord_x + self.__cell * x,  self.initial_cord_y + self.__cell * y, self.__cell, self.__cell), 1)
+                self.draw_block(screen, self.initial_cord_x + self.__cell * x,  self.initial_cord_y + self.__cell * y, self.__playing_field[x][y])
+
+    def draw_block(self, screen, cord_x, cord_y, color):
+        if color == "x":
+            return
+
+        pygame.draw.rect(screen, self.colors[color], (cord_x + 1, cord_y + 1, self.__cell - 1, self.__cell - 1), 0, 3)
+        pygame.draw.rect(screen, self.light_colors[color], (cord_x + 1, cord_y + 1, self.__cell - 4, self.__cell - 4), 0, 3)
+        pygame.draw.circle(screen, self.colors[color], (cord_x + self.__cell / 2, cord_y + self.__cell / 2), 5)
+
+class TetrisGame:
+    pygame.init()
+    __screen_x = 600
+    __screen_y = 500
+    __screen = pygame.display.set_mode((__screen_x, __screen_y))
+    __fps_clock = pygame.time.Clock()
+    __fps = 25
+    fall_speed = 0.5
+    pf = PlayingField(__screen_x, __screen_y)
 
 
-def drawBlock(cord_x, cord_y, color):
-    if color == "x":
-        return
+    def play(self):
 
-    pygame.draw.rect(screen, colors[color], (cord_x + 1, cord_y + 1, cell - 1, cell - 1), 0, 3)
-    pygame.draw.rect(screen, lightcolors[color], (cord_x + 1, cord_y + 1, cell - 4, cell - 4), 0, 3)
-    pygame.draw.circle(screen, colors[color], (cord_x + cell / 2, cord_y + cell / 2), 5)
+        running = True
+        self.__fps_clock.tick(self.__fps)
+        next_figure = Figure(pf.initialCord_x + (cell * field_w) / 2, pf.initialCord_y + 20)
+        falling_figure = Figure(pf.initialCord_x + (cell * field_w) / 2, pf.initialCord_y + 20)
 
+        pygame.display.set_caption("Tetris")
 
-def RandomFigure():
-    figuresCord = [
-        # линия
-        [[-2, 0], [-1, 0], [0, 0], [ 1, 0]],
-        # L-образная
-        [[-1, 1], [-1, 0], [0, 0], [ 1, 0]],
-        # обратная L-образная
-        [[ 1, 1], [-1, 0], [0, 0], [ 1, 0]],
-        # квадрат
-        [[-1, 1], [ 0, 1], [0, 0], [-1, 0]],
-        # Z-образная
-        [[ 1, 0], [ 1, 1], [0, 0], [-1, 0]],
-        # обратная Z-образная
-        [[ 0, 1], [-1, 0], [0, 0], [ 1, 0]],
-        # T-образная
-        [[-1, 1], [ 0, 1], [0, 0], [ 1, 0]],
-    ]
-    figure = random.choice(figuresCord)
+        while running:
 
-    for j in range(random.randint(0, 3)):
-        for i in range(len(figure)):
-            x_new = figure[i][1]
-            y_new = figure[i][0]
-            figure[i][0] = -x_new
-            figure[i][1] = y_new
-    return (figure, random.randint(0, len(colors)-1))
+            if falling_figure == None:
+                falling_figure = next_figure
+                next_figure = {"x": initialCord_x + (cell * field_w) / 2, "y": initialCord_y + 20, "Figure": NextFigure()}
 
-def NextFigure():
-    figure = RandomFigure()
-    for x, y in figure[0]:
-        drawBlock(initialCord_x + cell * (14 + x), initialCord_y + cell * (5 - y), figure[1])
-    return figure
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                elif event.type == pygame.KEYDOWN:
 
-def DrawFallingFigure(fallingFig):
-    figure = fallingFig["Figure"]
-    x = fallingFig["x"]
-    y = fallingFig["y"]
-    for block_x, block_y in figure[0]:
-        drawBlock(x + cell * block_x, y + cell * block_y, figure[1])
+                    # перемещение фигуры вправо и влево
+                    if event.key == pygame.K_LEFT:
+                        falling_figure.left_move()
 
-def checkPos(playingField, fallingFig, next_x=0, next_y=0):
-    # проверяет, находится ли фигура в границах стакана, не сталкиваясь с другими фигурами
-    figure = fallingFig["Figure"]
-    x = int((fallingFig["x"]-initialCord_x) / cell)
-    y = int((fallingFig["y"]-initialCord_y) / cell)
-    for block_x, block_y in figure[0]:
-        if y + block_y >=19 or playingField[x + block_x + next_x][y + block_y + next_y] != "x":
-            return False
-    return True
+                    elif event.key == pygame.K_RIGHT:
+                        falling_figure.right_move()
 
-def addToCup(playingField, fallingFig):
-    figure = fallingFig["Figure"]
-    x =  int((fallingFig["x"] - initialCord_x) / cell)
-    y =  int((fallingFig["y"] - initialCord_y) / cell)
-    for block_x, block_y in figure[0]:
-        playingField[x+block_x][y+block_y] = figure[1]
+                    # поворачиваем фигуру, если есть место
+                    elif event.key == pygame.K_UP:
+                        falling_figure.rotate()
 
-main()
+                    # ускоряем падение фигуры
+                    elif event.key == pygame.K_DOWN:
+                        falling_figure.fast_falling()
+
+                    # мгновенный сброс вниз
+                    elif event.key == pygame.K_RETURN:
+                        falling_figure.instant_falling()
+
+            pygame.display.flip()
+        pygame.quit()
+
+if __name__ == '__main__':
+    game = TetrisGame
+
