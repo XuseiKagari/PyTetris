3cfd95ffd22c91836097856e5c32220051a17b53
Niatomi <playervoker@gmail.com>
2025-08-21 11:58:55 +0400

    Changed collision check

--- src/figure.py
+++ src/figure.py
@@ -1,7 +1,7 @@
 import random, time
 from enum import Enum
 from typing import Callable
-from numpy import rot90, array
+from numpy import rot90, array, flip
 
 
 class Color(Enum):
@@ -51,7 +51,8 @@ class Figure:
             #  ['o', 'o', 'o']]
         ]
         figure = random.choice(figures_cord)
-        return figure
+        rotated_figure = flip(rot90(figure, 1), 0)
+        return rotated_figure.tolist()
 
     def get_figure(self):
         return self.__figure_type
@@ -59,40 +60,34 @@ class Figure:
     def set_figure(self, figure):
         self.__figure_type = figure
 
-    def set_x(self, x):
-        self.__x = x
+    def left_move(self, collision_func: Callable):
+        if self.collision_prob(collision_func, next_x=-1):
+            self.x -= 1
 
-    def set_y(self, y):
-        self.__y = y
+    def right_move(self, collision_func: Callable):
+        if self.collision_prob(collision_func, next_x=1):
+            self.x += 1
 
-    def left_move(self, playing_field):
-        if self.check_pos(playing_field, next_x=-1):
-            self.__x -= 1
-
-    def right_move(self, playing_field):
-        if self.check_pos(playing_field, next_x=1):
-            self.__x += 1
-
-    def rotate(self, playing_field, k=1):
+    def rotate(self, collision_func, k=1):
         rotated_figure = rot90(array(self.__figure_type), k)
-        # if self.check_rotate(playing_field, rotated_figure):
-        #     self.__figure_type = rotated_figure.tolist()
+        if self.collision_prob_rotate(collision_func, rotated_figure):
+            self.__figure_type = rotated_figure.tolist()
 
-    def fast_falling(self, is_collided_func: Callable, field: tuple[int, int]):
-        if self.check_pos(is_collided_func, field, next_y=1):
+    def fast_falling(self, collision_func: Callable):
+        if self.collision_prob(collision_func, next_y=1):
             self.y += 1
 
     def instant_falling(self, playing_field):
         y = 0
         for i in range(playing_field.field_h):
-            if not self.check_pos(playing_field, next_y=i):
+            if not self.collision_prob(playing_field, next_y=i):
                 break
             y = i
         self.__y += y
 
-    def free_fall(self, is_collided_func: Callable, field: tuple[int, int], fall_speed=0.5):
+    def free_fall(self, collision_func: Callable, fall_speed=0.5):
         if time.time() - self.__last_fall > fall_speed:  # свободное падение фигуры
-            if not self.check_pos(is_collided_func, field, next_y=1):
+            if not self.collision_prob(collision_func, next_y=1):
                 return False
             else:  # фигура пока не приземлилась, продолжаем движение вниз
                 self.y += 1
@@ -100,29 +95,25 @@ class Figure:
                 return True
         return True
 
-    def check_pos(self, is_collided_func, field: tuple[int, int], next_x=0, next_y=0):
-        w, h = field
+    def collision_prob(self, collision_func: Callable, next_x=0, next_y=0):
         for block_x in range(len(self.__figure_type)):
             for block_y in range(len(self.__figure_type[block_x])):
                 if self.__figure_type[block_x][block_y]:
-                    if self.x + block_x + next_x >= w or self.x + block_x + next_x < 0:
-                        return False
-                    elif self.y + block_y + next_y >= h or self.y + block_y + next_y < 0:
-                        return False
-                    print(self.x + block_x + next_x, self.y + block_y + next_y)
-                    if is_collided_func(self.x + block_x + next_x, self.y + block_y + next_y):
+                    if collision_func(
+                        self.x + block_x + next_x,
+                        self.y + block_y + next_y
+                    ):
                         return False
         return True
 
-    def check_rotate(self, playing_field, rotated_figure):
+    def collision_prob_rotate(self, collision_func: Callable, rotated_figure):
         for block_x in range(len(rotated_figure)):
             for block_y in range(len(rotated_figure[block_x])):
-                if rotated_figure[block_x][block_y] == 'x':
-                    if self.__x + block_x >= playing_field.field_w or self.__x + block_x < 0:
-                        return False
-                    elif self.__y + block_y >= playing_field.field_h or self.__y + block_y < 0:
-                        return False
-                    if playing_field.get_playing_field(self.__x + block_x, self.__y + block_y) != "X":
+                if rotated_figure[block_x][block_y]:
+                    if collision_func(
+                        self.x + block_x,
+                        self.y + block_y
+                    ):
                         return False
         return True
 
